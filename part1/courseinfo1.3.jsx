ce4cee117264d2e7317f386513d96ba90b373709
ilo-vecats <123067141+ilo-vecats@users.noreply.github.com>
2025-08-20 21:47:07 +0530

    Create courseinfo1.3.jsx

---
+++ part1/courseinfo1.3.jsx
@@ -0,0 +1,61 @@
+import React from "react"
+const Header=(props)=>{
+  return(
+    <h1>
+     {props.course}
+    </h1>
+  )
+}
+
+const Part =(props) =>{
+  return (
+    <p>
+      {props.p.name} {props.p.exercises}
+    </p>
+  )
+}
+
+const Content =(props)=>{
+  return(
+    <>
+    <Part p= {props.part1} />
+    <Part p = {props.part2}/>
+    <Part p = {props.part3}/>
+    </>
+  )
+
+}
+const Total= (props)=>{
+  return(
+    <p>
+    Number of exercises {props.exercise1+props.exercise2+props.exercise3}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1} part2 ={part2} part3={part3} />
+      <Total exercise1={part1.exercises} exercise2= {part2.exercises} exercise3={part3.exercises}/>
+      
+    </div>
+  )
+}
+
+export default App

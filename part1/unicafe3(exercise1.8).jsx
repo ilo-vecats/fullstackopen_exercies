365efb213dc8933a50e9313c234482acdca9992f
ilo-vecats <123067141+ilo-vecats@users.noreply.github.com>
2025-08-22 20:14:21 +0530

    Create unicafe3(exercise1.8).jsx

---
+++ part1/unicafe3(exercise1.8).jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useState } from 'react'
+
+const StatLine = ({text,value})=>(
+   <p><strong>{text}</strong> {value}</p>
+
+)
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handlegoodclick =()=> {
+    setGood(good+1);
+  
+  }
+  const handleneutralclick =()=> {
+    setNeutral(neutral+1);
+
+    
+  }
+  const handlebadclick =()=> {
+    setBad(bad+1);
+  
+  }
+  const average= ()=>{
+    //using {all} creates an object , so here use all , good directly
+    if(all>0)
+    return (good-bad)/all
+    return 0;
+  }
+
+  const positive =()=>{
+    if(all> 0)
+    return (good/(all))*(100)
+    return 0; 
+  }
+  //dont use all as a state, here because its not independent
+  //it can be derived from good, bad and neutral
+  const all = good+bad+neutral
+  
+
+
+  return (
+    <div>
+      <h1> give feedback</h1>
+      <button onClick={handlegoodclick}>good</button>
+      <button onClick={handleneutralclick}>neutral</button>
+      <button onClick={handlebadclick}>bad</button>
+      <h2>statistics</h2>
+      <StatLine text="good" value={good} />
+      <StatLine text="neutral" value={neutral}/>
+      <StatLine text="bad" value={bad}/>
+      <StatLine text="all" value={all} />
+      <StatLine text="average" value={average()}/>
+      <StatLine text="positive" value={positive()}/>
+
+    </div>
+  )
+}
+
+export default App

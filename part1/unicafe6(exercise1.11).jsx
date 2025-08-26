ba2a789b84dd8082dd67e0bd454301d7d2e5072d
ilo-vecats <123067141+ilo-vecats@users.noreply.github.com>
2025-08-26 17:23:34 +0530

    Create unicafe6(exercise1.11).jsx

---
+++ part1/unicafe6(exercise1.11).jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { useState } from 'react'
+
+const StatLine = ({text,value})=>{
+ 
+   return (
+  //do not create a new table here, otherwise incorrect html
+  //nested tables dont exist
+    <tr>
+   <td><strong>{text}</strong></td>
+   <td> {value}</td>
+   </tr>
+ 
+   )
+   
+
+
+}
+
+const Check=({good,neutral,bad})=>{
+  const all = good+neutral+bad
+  if(all== 0) {return <p>no feedback given</p>}
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+  return (
+  
+      <table>
+        <tbody>
+      <StatLine text="good" value={good}/>
+      <StatLine text="neutral" value={neutral}/>
+      <StatLine text="bad" value={bad} />
+      <StatLine text="all" value={all} />
+      <StatLine text="average" value={average} />
+      <StatLine text="positive" value={`${positive} %`} />
+       </tbody>
+      </table>
+    
+  )
+
+  
+
+
+}
+const Button=({handleClick,text})=>(
+  <button onClick={handleClick}>{text}</button>
+
+)
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handlegoodclick =()=> {
+    setGood(good+1);
+  
+  }
+  const handleneutralclick =()=> {
+    setNeutral(neutral+1);
+
+    
+  }
+  const handlebadclick =()=> {
+    setBad(bad+1);
+  
+  }
+  
+  return (
+    <div>
+      <h1> give feedback</h1>
+      <Button handleClick={handlegoodclick} text="good"/>
+      <Button handleClick={handleneutralclick} text="neutral"/>
+      <Button handleClick={handlebadclick} text ="bad"/>
+      <h2>statistics</h2>
+      <Check good={good} neutral={neutral} bad ={bad}/>
+
+
+    </div>
+  )
+}
+
+export default App

329b46958c4fe3f0bbabb7c7e55799d48c6484f2
ilo-vecats <123067141+ilo-vecats@users.noreply.github.com>
2025-08-26 18:04:06 +0530

    Create anecdotes2(exercise1.13).jsx

---
+++ part1/anecdotes2(exercise1.13).jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useState } from 'react'
+const Handleclick=({anecdotes,setSelected})=>{
+    function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
+}
+  const handle =()=>{
+    const ridx=getRandomInt(0,anecdotes.length)
+    setSelected(ridx);
+  }
+  return (
+  <button onClick={handle}>next anecdote</button>
+  )
+
+
+  }
+  const Handlevotes=({selected,votes,setVote})=>{
+
+    const handle=()=>{
+      const copy=[...votes]
+      copy[selected]+=1;
+      
+      setVote(copy)
+    }
+    return(
+
+      <button onClick={handle}>vote</button>
+
+    )
+
+  }
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  
+
+
+  const [selected, setSelected] = useState(0)
+  const [vote,setVote]=useState(Array(anecdotes.length).fill(0))
+
+
+
+  
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <div>
+      has {vote[selected]} votes
+      </div>
+      <div>
+       <Handlevotes selected={selected} votes={vote} setVote={setVote}/> 
+      <Handleclick anecdotes={anecdotes} setSelected={setSelected} />
+      </div>
+    </div>
+  )
+}
+
+export default App

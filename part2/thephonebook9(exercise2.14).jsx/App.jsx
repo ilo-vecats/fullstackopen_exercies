36a2d97541dcef809cc73459853767444b7504ea
ilo-vecats <123067141+ilo-vecats@users.noreply.github.com>
2025-09-06 23:08:18 +0530

    Create App.jsx

---
+++ part2/thephonebook9(exercise2.14).jsx/App.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import Filter from './Filter'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+import Services from './src/Services'
+
+const App = () => {
+  
+
+
+
+  const [newName, setNewName] = useState('')
+  const [newNumber,setNewNumber] = useState('')
+  const [filter,setFilter]=useState('')
+  const [persons,setPersons] =useState([])
+
+  const handleFilter=(event)=>{
+    console.log(event.target.value);
+    setFilter(event.target.value); 
+  }
+ 
+
+
+const hook=()=>{
+  Services
+  .getall()
+  .then(initialdata=>{
+    setPersons(initialdata)
+  })
+}
+useEffect(hook,[])
+
+const addnewName=(event)=>{
+  setNewName(event.target.value)
+
+}
+const addnewNumber=(event)=>{
+  setNewNumber(event.target.value)
+}
+
+const addcontact=(event)=>{
+  event.preventDefault()
+  const addperson={
+    name:newName,
+    number:newNumber,
+
+  }
+  Services
+  .create(addperson)
+  .then( newdata=>{
+  setPersons(persons.concat(newdata))
+  setNewName('')
+  setNewNumber('')
+  console.log(newdata)
+})
+
+
+
+}
+const removeperson=(ele)=>{
+  let decision = window.confirm(`remove ${ele.name}?`)
+  decision?
+ Services
+ .remove(ele.id)
+ .then(deleteddata=>{
+  console.log(deleteddata)
+  setPersons(persons.filter(e => e.id !== ele.id))
+ })
+ .catch(error=>(
+  console.log(error)
+ ))
+ : console.log(`not deleted ${ele.name}`)
+}
+ 
+  return (
+    <div>
+      <h2>Phonebook</h2>
+         <div>
+        filter shown with name
+        <input  value={filter} onChange={handleFilter}></input>
+       </div>
+      
+     <div>
+      <h2>Numbers</h2>
+       {persons.filter((ele)=>ele.name.toLowerCase().includes(filter.toLowerCase()))
+             .map((ele)=>(
+                    <div key ={ele.id}>
+                    <li >{ele.name} {ele.number}</li>
+                    <button  onClick={()=>removeperson(ele)}> delete contact</button>
+                    </div>
+             ))
+           }
+      </div>
+      <div>
+      <h2>add a contact</h2>
+      <form onSubmit={addcontact}>
+        <div>
+        <input value={newName} onChange ={addnewName} ></input>
+        </div>
+        <div>
+        <input value ={newNumber} onChange={addnewNumber}></input>
+        </div>
+        <button >submit</button>
+
+      </form>
+      </div>
+    
+
+     
+
+      
+    </div>
+  )
+}
+
+export default App
+

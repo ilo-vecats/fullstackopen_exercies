b5f4bd674131971ea60572854f8bbfee7c2cbb72
ilo-vecats <123067141+ilo-vecats@users.noreply.github.com>
2025-09-06 22:26:54 +0530

    Create Services.jsx

---
+++ part2/thephonebook8(exercise2.13).jsx/src/Services.jsx
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const getall=()=>{
+const req=axios.get('http://localhost:3001/persons')
+return req.then(response=>response.data);
+}
+
+const create=(addperson)=>{
+const request =axios.post('http://localhost:3001/persons',addperson)
+return request.then(response=>(response.data))
+}
+
+// is the same as {create:create,
+//                  getall:getall}
+export default {create,getall}

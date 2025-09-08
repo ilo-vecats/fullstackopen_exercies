b8f3ef41de08233d63b4deefdaebfea7055fdec2
ilo-vecats <123067141+ilo-vecats@users.noreply.github.com>
2025-09-08 17:21:13 +0530

    Create Notification.jsx

---
+++ part2/exercise2.16/Notification.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Notification = ({ notification }) => {
+  if (notification.text === null) {
+    return null
+  }
+  return (
+    <div className={notification.type}>
+      {notification.text}
+    </div>
+  )
+}
+export default Notification

b9e1b7cf82e051ecad3d191481fdb638b981c485
Rishika Dawra <123067141+ilo-vecats@users.noreply.github.com>
2025-10-18 20:24:34 +0530

    Create (exercise3.3)index.js

---
+++ part3/phonebook_backend/(exercise3.3)index.js
@@ -0,0 +1,63 @@
+//	So this line loads the Express framework and
+//  stores it in a variable named express.
+const express = require('express')
+
+//	When you call it (express()), 
+// it creates an Express application instance — 
+// often called app.
+const app = express()
+
+let notes = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons',(req,res)=>{
+    res.json(notes)
+   
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const note = notes.find(note => note.id === id)
+  
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.get('/api/persons/:id',(req,res)=>{
+    const id = req.params.id
+    const note = notes.find(note => notes.id === id)
+    
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
